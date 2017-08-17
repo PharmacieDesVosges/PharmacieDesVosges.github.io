8dc7c78818cba3f46c3bce814b05d0a1b5f1ca77
Alcide <paul.robin@etu.unistra.fr>
2017-08-17 15:09:08 +0200

    modification beauté

--- js/app.js
+++ js/app.js
@@ -38,9 +38,7 @@ view_pro.config(['$routeProvider',
 var sliderMP = angular.module('sliderMP',['ngAnimate']);
 
 sliderMP.controller('SliderControllerMP', function($scope) {
-    $scope.images=[{src:'logo.png',
-                   title:'logo'},
-                   {src:'ext01.jpg',
+    $scope.images=[{src:'ext01.jpg',
                     title:'Pic 1'},
                    {src:'ext02.jpg',
                     title:'Pic 2'},
