5d7386ef494110ac00a5367061425d12a3229fe4
Alcide <paul.robin@etu.unistra.fr>
2017-09-05 03:05:23 +0200

    ajustement

--- js/app.js
+++ js/app.js
@@ -165,5 +165,81 @@ sliderMM.directive('slidermat', function ($timeout) {
 var dtApp = angular.module('dtApp', []);
 
 dtApp.controller('dateController', function ($scope, $filter) {
-        $scope.result = $filter('date')(new Date(),'h');
+        var str_year = $filter('date')(new Date(),'yyyy');
+        var str_month = $filter('date')(new Date(),'M');
+        var str_day = $filter('date')(new Date(),'d');
+        var name_day = $filter('date')(new Date(),'EEEE');
+        var year = parseInt(str_year);
+        var month = parseInt(str_month)-1;
+        var day = parseInt(str_day);
+        if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            // Leap year
+            var feb = 29;
+        }
+        else {
+            // Not a leap year
+            var feb = 28;
+        }
+        var aggregateMonths = [0, // January
+                               31, // February
+                               31 + feb, // March
+                               31 + feb + 31, // April
+                               31 + feb + 31 + 30, // May
+                               31 + feb + 31 + 30 + 31, // June
+                               31 + feb + 31 + 30 + 31 + 30, // July
+                               31 + feb + 31 + 30 + 31 + 30 + 31, // August
+                               31 + feb + 31 + 30 + 31 + 30 + 31 + 31, // September
+                               31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30, // October
+                               31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31, // November
+                               31 + feb + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30, // December
+                              ];
+        var ntd = aggregateMonths[month]+day;
+        var c_day = 1;
+        if (name_day=='Friday' || name_day=='Saturday' || name_day=='Sunday' ){var nb_week = 0;}
+        else {var nb_week = 1;}
+        while(c_day < ntd){
+            var c_name_day = $filter('date')(new Date(new Date().getFullYear(),0,c_day),'EEEE');
+            if (c_name_day == 'Thursday'){
+                nb_week = nb_week+1;
+            }
+            c_day = c_day+1;
+        }
+        //calcule samedi de la semaine
+        if (name_day=='Saturday'){
+            ntd = -1;    
+        }
+        else{
+            if (name_day == 'Sunday'){
+                ntd = ntd + 6;
+            }
+            if (name_day == 'Monday'){
+                ntd = ntd + 5;
+            }
+            if (name_day == 'Tuesday'){
+                ntd = ntd + 4;
+            }
+            if (name_day == 'Wednesday'){
+                ntd = ntd + 3;
+            }
+            if (name_day == 'Thursday'){
+                ntd = ntd + 2;
+            }
+            if (name_day == 'Friday'){
+                ntd = ntd + 1;
+            }
+        }
+        if(ntd>0){
+            var date_samedi = "le "+$filter('date')(new Date(new Date().getFullYear(),0,ntd),'d/M/y');
+        }
+        else{
+            var date_samedi = "aujourd'hui"
+        }
+        if (nb_week % 2 == 0){
+            $scope.result = "Ouverte";
+        }
+        else{
+            $scope.result = "Fermée";
+        }
+        $scope.sam_date = date_samedi;
+
     });
